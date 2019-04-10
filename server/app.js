a49285b5afd7088f918d218593c8d41b3c28afc7
ele-cren <ele-cren@student.42.fr>
2019-04-10 15:49:20 +0200

    mysql co

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 import Server from './server'
 import Api from './api/api'
+import mysql from 'mysql'
+require('dotenv').config()
 
 const server = new Server()
+
+export const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'matcha'
+});
 
 server.start()
 
