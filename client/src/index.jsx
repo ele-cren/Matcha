3c8e40a8f72be8936b932af1fca1d2b7a0cbf4e3
Erwan <lecren.erwan@gmail.com>
2019-04-29 19:42:10 +0200

    css file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import React from 'react'
 import { render } from 'react-dom'
-import App from './containers/App'
+import App from './containers/App/App'
 import { Provider } from 'react-redux'
 import store from './store'
 
