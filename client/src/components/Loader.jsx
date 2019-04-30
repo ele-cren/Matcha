8501f175855abf3b13acc64af10064d036f39082
Erwan <lecren.erwan@gmail.com>
2019-04-30 16:00:04 +0200

    heart loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 class Loader extends React.Component {
   render () {
     return (
-      <div class="gooey">
-        <span class="dot"></span>
-        <div class="dots">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
+      <div className='heart'></div>
     )
   }
 }
