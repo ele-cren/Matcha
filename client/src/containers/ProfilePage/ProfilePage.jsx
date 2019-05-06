f994ee026ab1ed4f951b563c2030606186e327d8
Erwan <lecren.erwan@gmail.com>
2019-05-06 19:00:50 +0200

    profil first try

--- client/src/containers/ProfilePage/ProfilePage.jsx
+++ client/src/containers/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { cleanErrors } from '../../actions/errorsActions/errorsActions'
-import { MDBCarousel, MDBIcon, MDBCarouselInner, MDBCarouselItem, MDBView, MDBContainer, MDBCarouselCaption } from
+import { MDBCarousel, MDBCardText, MDBBtn, MDBIcon, MDBCarouselInner, MDBCarouselItem, MDBCardTitle, MDBView, MDBCardBody, MDBContainer, MDBCarouselCaption, MDBCard, MDBCol } from
 "mdbreact"
 import Loader from '../../components/Loader'
 import * as style from './ProfilePage_style'
@@ -32,23 +32,80 @@ class ProfilePage extends React.Component {
     }
   }
   
+  getGenre (genreNum) {
+    switch (genreNum) {
+      case 1:
+        return 'Male'
+      case 2:
+        return 'Female'
+      default:
+        return 'Male'
+    }
+  }
+
+  getOrientation (orientationNum) {
+    switch (orientationNum) {
+      case 1:
+        return 'Straight'
+      case 2:
+        return 'Bisexual'
+      case 3:
+        return 'Lesbian'
+      case 4:
+        return 'Gay'
+      default:
+        return 'Bisexual'
+    }
+  }
 
   render () {
     return (
-      <MDBContainer style={ style.container } className="mt-3" >
-        <MDBCarousel
-          activeItem={1}
-          length={ this.props.profile.pictures.length }
-          showIndicators={true}
-          showControls={ true }
-          className="z-depth-1"
-          style={ style.carousel }
-        >
-          <MDBCarouselInner>
-            { this.props.profile.pictures.map((x, i) => <MyCarouselItem key={ i } url={ x.url } id={ i + 1 } isMain={ x.main } />) }
-          </MDBCarouselInner>
-        </MDBCarousel>
+      <MDBContainer style={ style.container }>
+        <MDBCol md="8">
+          <MDBCard style={ style.card }>
+            <MDBCarousel
+              activeItem={1}
+              length={ this.props.profile.pictures.length }
+              showIndicators={true}
+              showControls={ true }
+              className="z-depth-1"
+              style={ style.carousel }>
+              <MDBCarouselInner>
+                { this.props.profile.pictures.map((x, i) => <MyCarouselItem key={ i } url={ x.url } id={ i + 1 } isMain={ x.main } />) }
+              </MDBCarouselInner>
+            </MDBCarousel>
+            <h2 className='mt-3'>{ this.props.profile.mainInformations.first_name + ' ' + this.props.profile.mainInformations.last_name }</h2>
+            <MDBCardBody className="text-center">
+              <MDBCardText style={ style.bio }>
+                { this.props.profile.informations.bio }
+              </MDBCardText>
+              <div style={ style.personalInfos }>
+                <p style={ style.genre }>{ this.getGenre(this.props.profile.informations.genre) }</p>
+                <p style={ style.orientation }>{ this.getOrientation(this.props.profile.informations.orientation) }</p>
+              </div>
+              <MDBBtn color="elegant" className="mt-4">Edit</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
       </MDBContainer>
+
+
+
+      // <MDBContainer style={ style.container } className="mt-3" >
+      //   <MDBCarousel
+      //     activeItem={1}
+      //     length={ this.props.profile.pictures.length }
+      //     showIndicators={true}
+      //     showControls={ true }
+      //     className="z-depth-1"
+      //     style={ style.carousel }
+      //   >
+      //     <MDBCarouselInner>
+      //       { this.props.profile.pictures.map((x, i) => <MyCarouselItem key={ i } url={ x.url } id={ i + 1 } isMain={ x.main } />) }
+      //     </MDBCarouselInner>
+      //   </MDBCarousel>
+      //   <h2>{ this.props.profile.mainInformations.first_name + ' ' + this.props.profile.mainInformations.last_name }</h2>
+      // </MDBContainer>
     )
   }
 }
--- client/src/containers/ProfilePage/ProfilePage_style.js
+++ client/src/containers/ProfilePage/ProfilePage_style.js
@@ -4,13 +4,55 @@ export const picture = {
 
 export const container = {
   display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center'
 }
 
+export const card = {
+  margin: '20px 0 0 0',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  backgroundColor: '#fcfdff'
+}
+
+export const genre = {
+  border: '1px solid black',
+  width: '100px',
+  padding: '5px 0 5px 0',
+  backgroundColor: '#87a7db',
+  margin: '0 2px 0 2px'
+}
+
+export const orientation = {
+  border: '1px solid black',
+  width: '100px',
+  padding: '5px 0 5px 0',
+  backgroundColor: '#a3e088',
+  margin: '0 2px 0 2px'
+}
+
+export const personalInfos = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
+
+export const bio = {
+  border: '1px solid #d6d6d6',
+  padding: 20,
+  borderRadius: 3,
+  fontSize: '1.2em',
+  backgroundColor: '#fcf9f9'
+}
+
 export const carousel = {
   width: '200px',
-  height: '200px'
+  height: '200px',
+  marginTop: 10
 }
 
 export const caption = {
